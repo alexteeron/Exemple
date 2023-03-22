445a3d2d0970b8e8f22099693fed3cf19df05d24
Alexandr Kim <corpicorns@yandex.ru>
2023-03-23 02:46:46 +0300

    add Exemple Condition If Else

---
+++ Exemple005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя:");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша!");
+}else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
